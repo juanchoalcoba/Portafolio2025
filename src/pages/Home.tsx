070b748aa2d7bbbca534476c993e56ec95014cb5
juanchoalcoba <juanchoalcoba@gmail.com>
2025-06-07 11:27:03 -0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,21 +4,23 @@ import Works from "../components/Works";
 
 const Home = () => {
   return (
-    <section className="relative flex-col overflow-hidden h-auto w-full font-roboto flex items-center justify-center pt-12 text-white">
-      {/* Fondo nebuloso con gradientes y manchas */}
-      <div className="absolute inset-0 -z-10 bg-gradient-to-tr from-gray-900 via-gray-950 to-gray-900">
+    <>
+      {/* Fondo nebuloso fijo con gradientes y manchas, detrás de todo */}
+      <div className="fixed inset-0 -z-10 bg-gradient-to-tr from-gray-900 via-gray-950 to-gray-900">
         <div className="absolute top-20 left-10 w-96 h-96 bg-blue-900 opacity-30 rounded-full blur-3xl" />
         <div className="absolute bottom-10 right-20 w-96 h-96 bg-blue-900 opacity-30 rounded-full blur-3xl" />
         <div className="absolute top-1/2 left-1/2 w-[600px] h-[600px] bg-gray-800 opacity-20 rounded-full blur-[120px] -translate-x-1/2 -translate-y-1/2" />
       </div>
 
-      {/* Contenido con padding interno sin afectar el ancho total */}
-      <div className="w-full px-6 max-w-screen-xl">
-        <About />
-        <Experience />
-        <Works />
-      </div>
-    </section>
+      {/* Contenedor principal del contenido */}
+      <section className="flex flex-col items-center justify-start w-full min-h-screen font-roboto pt-20 px-6 text-white">
+        <div className="w-full flex flex-col justify-center items-center">
+          <About />
+          <Experience />
+          <Works />
+        </div>
+      </section>
+    </>
   );
 };
 
